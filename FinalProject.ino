2bfae4eebfd84a598d99a3efb184ac253ce3d828
PktHammer <44016363+PktHammer@users.noreply.github.com>
2020-05-08 20:03:05 -0400

    Update FinalProject.ino

--- FinalProject.ino
+++ FinalProject.ino
@@ -1,36 +1,41 @@
 #define DEBUG false
-String AP_SSID = "SSID";
-String AP_PASSWORD = "PASSWORD";
+#include <SoftwareSerial.h>
+String AP_SSID = "Whenever You Think";
+String AP_PASSWORD = "30443044";
 int RST_WAIT_TIME = 5000;
 int WAIT_TIME = 5000;
 boolean is_wifi_setup = false;
+SoftwareSerial wifiComm(2,3); // RX, TX
+
+
 void setup() {
   // put your setup code here, to run once:
-  // Serial.begin(115200);
+  Serial.begin(9600);
+  wifiComm.begin(9600);
+
   pinMode(LED_BUILTIN, OUTPUT);
-  
+  Serial.print("Starting\r\n");
 }
 
 void loop() {
   if(!is_wifi_setup){
     delay(2000);
-    blink(10);
+    blink_n(10);
     delay(2000);
     if(sendData("AT+RST\r\n", RST_WAIT_TIME, DEBUG)){
-      blink(4);
+      blink_n(4);
     }
-  
     if(sendData("AT+CWMODE=1\r\n",WAIT_TIME, DEBUG)){
-      blink(6);
+      blink_n(6);
     }
     if(sendData("AT+CWJAP=\"" + AP_SSID + "\",\"" + AP_PASSWORD + "\"\r\n", WAIT_TIME, DEBUG)){
-      blink(8);
+      blink_n(8);
     }
     if(sendData("AT+CIPMUX=1\r\n", WAIT_TIME, DEBUG)){
-      blink(10);
+      blink_n(10);
     }
     if(sendData("AT+CIPSERVER=1,80\r\n", WAIT_TIME, DEBUG)){
-      blink(12);
+      blink_n(12);
     }
     is_wifi_setup = true;
   }
@@ -42,27 +47,27 @@ void loop() {
 //    sendData("AT+CIPSEND=0,21\r\n", 2000, DEBUG);
 //    sendData("<h1>Hello World!</h1>\r\n", 2000, DEBUG);
 //    sendData("AT+CIPCLOSE=0\r\n", 2000, DEBUG);
-//  } 
+//  }
 }
 
 
 boolean sendData(String command, const int timeout, boolean debug) {
+  Serial.print("Attempting to send " + command + "\r\n");
+  blink_n(2);
   boolean was_serial_available = false;
   String response = "";
-  Serial.print(command);
-    long int time = millis();
-//  while( (time+timeout) > millis()){
-    while(!Serial.available()){
-      blink(1);
-    }
-    while(Serial.available()){
+  wifiComm.print(command);
+  wifiComm.listen();
+  long int time = millis();
+  while( (time+timeout) > millis()){
+    while(wifiComm.available() > 0){
       was_serial_available = true;
-      //Serial.print("We hit the codeblock!\r\n");
+      Serial.print("We hit the codeblock!\r\n");
       // The esp has data so display its output to the serial window
-      char c = Serial.read(); // read the next character.
+      char c = wifiComm.read(); // read the next character.
       response+=c;
-      }
-//  }
+    }
+  }
   
   if(debug)
   {
@@ -72,11 +77,18 @@ boolean sendData(String command, const int timeout, boolean debug) {
 }
 
 // the loop function runs over and over again forever
-void blink(int number_of_blinks) {
+void blink_n(int number_of_blinks) {
   for(int i=0;i<number_of_blinks;i++){
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(200);                       // wait for a second
+    delay(100);                       // wait for a second
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(200);                       // wait for a second
+    delay(100);                       // wait for a second
   }
+}
+
+void blink_error(){
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(50);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(50);
 }
